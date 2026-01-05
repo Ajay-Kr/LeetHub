2462632928f2e4370f09d240d22eacc53985b68c
Ajay Kumar <55989070+Ajay-Kr@users.noreply.github.com>
2026-01-06 00:22:50 +0530

    Time: 305 ms (53.85%), Space: 64.6 MB (84.62%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.js
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.js
@@ -5,19 +5,43 @@
  */
  smallestTrimmedNumbers = function(nums, queries) {
     const answer = [];
-    for(let i = 0; i < queries.length; i++){
-      const [k, toTrim] = queries[i];
-      const strLen = nums[0].length;
-      const trimmedArray = nums
-                              .map((n, idx) => [BigInt(n.substring(strLen-toTrim)), idx])
+    const maxStringLength = nums[0].length;
+    const uniqueTrims = {};
+    queries.forEach(q => uniqueTrims[q[1]] = {subStringStart: maxStringLength-q[1]});
+    Object.keys(uniqueTrims).forEach(trim => {
+      const uniqueTrimArr = nums
+                              .map((n, idx) => [BigInt(n.substring(uniqueTrims[trim]['subStringStart'])), idx])
                               .sort((a, b) => {
                                 if(a[0] == b[0]) {
-                                  return a[1] < b[1] ? -1 : a[1] > a[1] ? 1 : 0;
+                                  return a[1] < b[1] ? -1 : a[1] > b[1] ? 1 : 0;
                                 }
                                 return a[0] < b[0] ? -1 : a[0] > a[0] ? 1 : 0;
                               });
-      answer.push(trimmedArray[k - 1][1])
-    }
+      uniqueTrims[trim]["array"] = uniqueTrimArr;
+      
+    })
+
+    queries.forEach(q => {
+      const trim = q[1];
+      const index = q[0] -1;
+      answer.push(uniqueTrims[trim]["array"][index][1])
+    })
+
+
+
+    // for(let i = 0; i < queries.length; i++){
+    //   const [k, toTrim] = queries[i];
+    //   const strLen = nums[0].length;
+    //   const trimmedArray = nums
+    //                           .map((n, idx) => [n.substring(strLen-toTrim), idx])
+    //                           .sort((a, b) => {
+    //                             if(a[0] == b[0]) {
+    //                               return a[1] < b[1] ? -1 : a[1] > b[1] ? 1 : 0;
+    //                             }
+    //                             return a[0] < b[0] ? -1 : a[0] > a[0] ? 1 : 0;
+    //                           });
+    //   answer.push(trimmedArray[k - 1][1])
+    // }
 
     return answer;
 };
