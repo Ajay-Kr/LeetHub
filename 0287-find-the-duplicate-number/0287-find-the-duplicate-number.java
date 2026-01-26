24c0a3a54d01284c8c992a286f1953bf28862719
Ajay Kumar <55989070+Ajay-Kr@users.noreply.github.com>
2026-01-26 23:59:53 +0530

    Time: 4 ms (90.86%), Space: 83.1 MB (40.04%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -6,14 +6,34 @@ class Solution {
         // int ans = sum - shouldHaveSum;
         // return ans;
 
-        int[] freqArr = new int[nums.length];
-        for(int i = 0; i < nums.length; i++) {
-          if(freqArr[nums[i]] > 0) {
-            return nums[i];
-          }
-          freqArr[nums[i]]++;
-        }
+        // int[] freqArr = new int[nums.length];
+        // for(int i = 0; i < nums.length; i++) {
+        //   if(freqArr[nums[i]] > 0) {
+        //     return nums[i];
+        //   }
+        //   freqArr[nums[i]]++;
+        // }
 
-        return -1;
+        // return -1;
+
+        int slow=0;
+        int fast = 0;
+
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if (slow == fast) {
+                break;
+            }
+        }
+        int slow2 = 0;
+        while (true) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+            if(slow == slow2){
+                return slow;
+            }
+        }
     }
 }
