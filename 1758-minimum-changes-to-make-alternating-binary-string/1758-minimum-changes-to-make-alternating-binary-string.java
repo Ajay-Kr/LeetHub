4b104e8d9e02526b4bb9223f91c87b49d5cc7d79
Ajay Kumar <55989070+Ajay-Kr@users.noreply.github.com>
2026-03-06 00:09:27 +0530

    Time: 6 ms (33.91%), Space: 43.9 MB (29.95%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -1,17 +1,13 @@
 class Solution {
     public int minOperations(String s) {
         int sLen = s.length();
-        String mask1 = "";
-        String mask2 = "";
-        for(int i = 0; i < sLen; i++) {
-          mask1 += i%2 == 0 ? "0" : "1";
-          mask2 += i%2 == 0 ? "1" : "0";
-        }
         
         int count1=0, count2=0;
         for(int i = 0; i < sLen; i++) {
-          count1 += (s.charAt(i) ^ mask1.charAt(i));
-          count2 += (s.charAt(i) ^ mask2.charAt(i));
+          char mask1 = i%2 == 0 ? '0' : '1';
+          char mask2 = i%2 == 0 ? '1' : '0';
+          count1 += (s.charAt(i) ^ mask1);
+          count2 += (s.charAt(i) ^ mask2);
         }
 
         if(count1 < count2) return count1;
